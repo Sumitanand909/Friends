470203d6b3b7ef8cf90cffe566dc1c2590c826aa
SUMIT ANAND <127883732+Sumitanand909@users.noreply.github.com>
2024-08-16 21:13:23 +0530

    script.js

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 var isstatus = document.querySelector('h5')
 var addFriend = document.querySelector('#add')
 var removeFriend = document.querySelector('#remove')
+var img = document.querySelector('img')
 
 addFriend.addEventListener("click",function(){
     isstatus.innerHTML = 'Request Sent'
     isstatus.style.color = 'grey'
     setTimeout(a,6000)
     setTimeout(b,8000)
-   
+    setTimeout(c,9000)
 })
+
 function a(){
 isstatus.innerHTML = 'Request Accepted'
 isstatus.style.color = '#66FF00'
@@ -19,9 +21,14 @@ function b(){
     isstatus.style.color = 'green'
 }
 
+function c(){
+img.src = 'https://plus.unsplash.com/premium_photo-1661421746164-b8b53de3bd4e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+}
+
 removeFriend.addEventListener("click",function(){
     isstatus.innerHTML = 'Friendship break'
     isstatus.style.color = 'red'
+    img.src = 'https://images.unsplash.com/photo-1592046285097-6cdf4daf0d69?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
 })
 
 
